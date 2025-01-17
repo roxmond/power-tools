2c89991f7a81c6f6305fbc9d49a2bedf36dea922
teo <dteconomidis@gmail.com>
2025-01-17 13:14:17 +0200

    feat: meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          property="og:url"
+          content="https://power-tools-gilt.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Power Tools" />
+        <meta
+          property="og:description"
+          content="Utilizing APIs, Websockets and Maths to create powerfull tools we need every day."
+        />
+        <meta property="og:image" content="/pt-img.png" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
